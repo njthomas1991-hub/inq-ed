f2ebc16c92e0586b52dc4dee190cd08a5bd9f7a4
YourGitHubUsername <n.j.thomas1991@gmail.com>
2026-01-28 11:27:04 +0000

    folder structure and images

---
+++ core/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def login(request):
+    return render(request, 'core/base.html')
+
+def logout(request):
+    return render(request, 'core/base.html')
+
+def register(request):
+    return render(request, 'core/base.html')
+
+def about(request):
+    return render(request, 'core/base.html')
+
+def wonderworld(request):
+    return render(request, 'core/base.html')
+
+def questopia(request):
+    return render(request, 'core/base.html')
+
+def kindlewick(request):
+    return render(request, 'core/base.html')
+
+def pricing(request):
+    return render(request, 'core/base.html')
+
+def teacher_hub(request):
+    return render(request, 'core/base.html')
+
+def contact(request):
+    return render(request, 'core/base.html')
+
+def signup(request):
+    return render(request, 'core/base.html')
+
+@api_view(['GET'])
+def hello(request):
+    return Response({"message": "Hello from Django!"})
+
